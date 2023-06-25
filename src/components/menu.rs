7e87d37bff4d3a7fdf0bf0a2e7b4d5acfcfdb470
Valentin271 <valentin.d77@free.fr>
2023-06-25 10:40:14 +0200

    refactor: menu in an aside nav

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -11,17 +11,21 @@ pub fn Menu(cx: Scope) -> Element {
     };
 
     cx.render(rsx!(
-        ul {
-            li {
-                Link { to: "/profile", "Profile" }
+        aside {
+            nav {
+                ul {
+                    li {
+                        Link { to: "/profile", "Profile" }
+                    }
+                    li {
+                        Link { to: "/traject/create", "Créer un trajet" }
+                    }
+                    li {
+                        Link { to: "/traject/search", "Rechercher un trajet" }
+                    }
+                    li { a { onclick: disconnect, href: "#", "Déconnexion" } }
+                }
             }
-            li {
-                Link { to: "/traject/create", "Créer un trajet" }
-            }
-            li {
-                Link { to: "/traject/search", "Rechercher un trajet" }
-            }
-            li { a { onclick: disconnect, href: "#", "Déconnexion" } }
         }
     ))
 }
