fc49e38053e237bf91d1be58733253243d8ffdaa
Valentin271 <valentin.d77@free.fr>
2023-06-25 18:06:51 +0200

    style: format

--- src/components.rs
+++ src/components.rs
@@ -8,6 +8,6 @@ pub mod vehicle_card;
 pub use add_vehicle::AddVehicle;
 pub use alert::Alert;
 pub use menu::Menu;
+pub use navbar::Navbar;
 pub use trip_card::TripCard;
 pub use vehicle_card::VehicleCard;
-pub use navbar::Navbar;
