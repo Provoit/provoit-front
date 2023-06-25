b106064822d25c68f91e1024cb08263c87e2dddf
Valentin271 <valentin.d77@free.fr>
2023-06-25 16:48:30 +0200

    feat: trip card

--- src/components.rs
+++ src/components.rs
@@ -1,8 +1,11 @@
 pub mod add_vehicle;
 pub mod alert;
 pub mod menu;
+pub mod trip_card;
 pub mod vehicle_card;
 
 pub use add_vehicle::AddVehicle;
 pub use alert::Alert;
 pub use menu::Menu;
+pub use trip_card::TripCard;
+pub use vehicle_card::VehicleCard;
