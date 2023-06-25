3c91bf810df844a91bb13bd1ff80ea6a6f6d9fd2
Valentin271 <valentin.d77@free.fr>
2023-06-25 16:48:43 +0200

    feat: basic trip list

--- src/pages/search_trip_page.rs
+++ src/pages/search_trip_page.rs
@@ -1,5 +1,27 @@
 use dioxus::prelude::*;
+use provoit_types::models::trips::Trip;
+
+use crate::{components::TripCard, utils::request};
 
 pub fn SearchTripPage(cx: Scope) -> Element {
-    cx.render(rsx! { h1 { text_align: "center", "Chercher un trajet" } })
+    let trips = use_future(cx, (), |_| async move {
+        request::get("/trips/search")
+            .await?
+            .json::<Vec<Trip>>()
+            .await
+    });
+
+    cx.render(rsx! {
+        main { class: "container",
+            h1 { text_align: "center", aria_busy: trips.value().is_none(), "Chercher un trajet" }
+            match trips.value() {
+                Some(Ok(trips)) => {
+                    rsx!(trips.iter().map(|tr| {
+                        rsx!(TripCard { trip: tr.to_owned() })
+                    }))
+                },
+                _ => rsx!("")
+            }
+        }
+    })
 }
