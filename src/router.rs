5246b58fb07004190a9d5e4232e1affc074d2801
Valentin271 <valentin.d77@free.fr>
2023-06-25 21:56:49 +0200

    feat: redirect to login if unlogged

--- src/router.rs
+++ src/router.rs
@@ -3,18 +3,24 @@ use dioxus_router::{Redirect, Route};
 
 use crate::{
     components::Navbar,
-    pages::{CreateUserPage, LoginPage, PageNotFound, ProfilePage},
+    pages::{CreateUserPage, LoginPage, PageNotFound, ProfilePage}, auth::Auth,
 };
 
 pub fn Router(cx: Scope) -> Element {
+    let auth = use_shared_state::<Auth>(cx).unwrap();
+
     cx.render(rsx! {
-        dioxus_router::Router {
+        dioxus_router::Router { 
             Navbar {}
             Route { to: "/login", LoginPage {} }
             Route { to: "/user/create", CreateUserPage {} }
             Route { to: "/profile", ProfilePage {} }
             Route { to: "", PageNotFound {} }
-            Redirect { to: "/login" }
+            if auth.read().user.is_none() {
+                rsx!(Redirect { to: "/login" })
+            } else {
+                rsx!(Redirect { to: "/trip/search" })
+            }
         }
     })
 }
